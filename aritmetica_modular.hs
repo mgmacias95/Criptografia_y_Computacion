a966708f4df4feed71edc7eb1bbeb27972ba2346
mgmacias95 <mgmacias95@correo.ugr.es>
2017-02-23 15:12:58 +0100

    euclides ext working

--- aritmetica_modular.hs
+++ aritmetica_modular.hs
@@ -10,21 +10,22 @@ extended_euclides a b = extended_euclides_tabla a b 1 0 0 1
 
 -- Función auxiliar para el algoritmo extendido de euclides
 extended_euclides_tabla :: Int -> Int -> Int -> Int -> Int -> Int -> (Int, Int, Int)
+extended_euclides_tabla a 0 x2 _ y2 _ = (a, x2, y2)
 extended_euclides_tabla a b x2 x1 y2 y1 = extended_euclides_tabla b r x1 x y1 y
-			where
-				q = a `div` b
-				r = a - q*b
-				x = x2 - q*x1
-				y = y2 - q*y1
+            where
+                q = a `div` b
+                r = a - q*b
+                x = x2 - q*x1
+                y = y2 - q*y1
 
 
 {-
 Ejercicio 2
 
-Usando el ejercicio anterior, escribe una función que calcule a^{-1} mod b para cualesquiera a,b \in Z que
+Usando el ejercicio anterior, escribe una función que calcule a^-1 mod b para cualesquiera a,b \in Z que
 sean primos relativos.
 -}
 inverse :: Int -> Int -> Int
 inverse a b = i `mod` b
-	where
-		i = extended_euclides a b !! 1
+    where
+        (_,i,_) = extended_euclides a b
