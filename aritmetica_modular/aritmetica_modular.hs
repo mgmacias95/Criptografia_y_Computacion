0d399d4734365f0ce88fb4c9541262ef9d990259
mgmacias95 <mgmacias95@correo.ugr.es>
2017-03-02 15:03:58 +0100

    using Integral

--- aritmetica_modular/aritmetica_modular.hs
+++ aritmetica_modular/aritmetica_modular.hs
@@ -8,12 +8,12 @@ Implementa el algoritmo extendido de Euclides para el cálculo del máximo comú
 dados dos enteros'a y b, encuentra u, v \in Z tales que au + bv es el máximo común divisor 
 de a y b
 -}
-extended_euclides :: Integer -> Integer -> (Integer, Integer, Integer)
+extended_euclides :: Integral a => a -> a -> (a, a, a)
 extended_euclides a 0 = (a, 1, 0)
 extended_euclides a b = extended_euclides_tabla a b 1 0 0 1
 
 -- Función auxiliar para el algoritmo extendido de euclides
-extended_euclides_tabla :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> (Integer, Integer, Integer)
+extended_euclides_tabla :: Integral a => a -> a -> a -> a -> a -> a -> (a, a, a)
 extended_euclides_tabla a 0 x2 _ y2 _ = (a, x2, y2)
 extended_euclides_tabla a b x2 x1 y2 y1 = extended_euclides_tabla b r x1 x y1 y
             where
@@ -29,7 +29,7 @@ Ejercicio 2
 Usando el ejercicio anterior, escribe una función que calcule a^-1 mod b para cualesquiera 
 a,b \in Z que sean primos relativos.
 -}
-inverse :: Integer -> Integer -> Integer
+inverse :: Integral a => a -> a -> a
 inverse a b
     | r == 1 = i `mod` b
     | otherwise = -1
@@ -42,14 +42,14 @@ Ejercicio 3
 Escribe una función que calcule a^b mod n para cualquiera a, b y n enteros positivos. 
 La implementación debería tener en cuenta la representación binaria de b.
 -}
-exponential_zn :: Integer -> Integer -> Integer -> Integer
+exponential_zn :: Integral a => a -> a -> a -> a
 exponential_zn _ 0 _ = 1
 exponential_zn a 1 _ = a
 exponential_zn a k n = exponential_zn_aux a k n 1 ki
                 where
                     ki = k `mod` 2
 
-exponential_zn_aux :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+exponential_zn_aux :: Integral a => a -> a -> a -> a -> a -> a
 exponential_zn_aux a 1 n b _ = a*b `mod` n
 exponential_zn_aux a k n b ki
     | ki == 0   = exponential_zn_aux a0 k0 n b ki0
@@ -68,7 +68,7 @@ primo usando el método de Miller-Rabin
 -}
 
 -- Descompone un número p en 2^u * s
-descomposicion_2us :: Integer -> Integer -> (Integer, Integer)
+descomposicion_2us :: Integral a => a -> a -> (a, a)
 descomposicion_2us p t 
     | m == 0    = descomposicion_2us u s
     | otherwise = (t, p)
@@ -79,11 +79,11 @@ descomposicion_2us p t
     
 
 -- realiza el test de miller rabin 10 veces
-miller_rabin :: Integer -> Bool
+miller_rabin :: (Integral a, Random a) => a -> Bool
 miller_rabin p = foldl1 (&&) (map miller_rabin_once (take 10 (repeat p))) 
 
 -- comprueba que p >= 5
-miller_rabin_once :: Integer -> Bool
+miller_rabin_once :: (Integral a, Random a) => a -> Bool
 miller_rabin_once p
     | p < 5                = error "Imposible aplicar test para p < 5"
     | b == 1 || b == (p-1) = True
@@ -94,7 +94,7 @@ miller_rabin_once p
                 b     = exponential_zn a s p
 
 -- realiza el test de miller rabin
-miller_rabin_ok :: Integer -> Integer -> Integer -> Integer -> Integer -> Bool
+miller_rabin_ok :: (Integral a, Random a) => a -> a -> a -> a -> a -> Bool
 miller_rabin_ok p u a i b
     | a == 1 && b == (p-1) = True
     | a == 1 && b /= (p-1) = False
@@ -109,7 +109,7 @@ Ejercicio 5
 Implementa el algoritmo paso enano-paso gigante para el cálculo de logaritmos
 discretos en Zp.
 -}
-shanks :: Integer -> Integer -> Integer -> [Integer]
+shanks :: Integral a => a -> a -> a -> [a]
 shanks a c p = tabT
     where
         s    = ceiling (sqrt (fromIntegral p))
