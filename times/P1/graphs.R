8ee052d256914276610ddd71a6148560f154753a
mgmacias95 <mgmacias95@correo.ugr.es>
2017-04-03 16:32:13 +0200

    finished R script

--- times/P1/graphs.R
+++ times/P1/graphs.R
@@ -3,9 +3,14 @@ representa_datos <- function(datos, filename) {
     library(reshape2)
     d_dat <- melt(datos, id.vars="Primo")
     names(d_dat) <- c("Primo", "Version", "Tiempo")
-    ggplot(d_dat, aes(Primo,Tiempo)) + geom_line(aes(color=Version))
+    ggplot(d_dat, aes(x=Primo,y=Tiempo,color=Version)) + geom_line() + geom_point()
     ggsave(paste(filename, ".png", sep=""))
 }
 
 miller_rabin <- read.csv("miller_rabin.csv")
-representa_datos(datos=miller_rabin, filename="miller_rabin.png")
+shanks <- read.csv("baby_s_giant_s.csv")
+factorizar <- read.csv("factorizar.csv")
+
+representa_datos(datos=miller_rabin, filename="miller_rabin")
+representa_datos(datos=shanks, filename="shanks")
+representa_datos(datos=factorizar, filename="factorizar")
