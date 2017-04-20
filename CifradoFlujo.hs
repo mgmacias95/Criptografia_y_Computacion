badf92dd32b4ea0b202813e3b6cb1fd84521e199
mgmacias95 <mgmacias95@correo.ugr.es>
2017-04-20 11:16:43 +0100

    corrección

--- CifradoFlujo.hs
+++ CifradoFlujo.hs
@@ -11,8 +11,7 @@ golomb s = cond1 && cond2
         n_ones  = fromIntegral $ sum s
         n_zeros = (length s) - n_ones
         cond1   = abs (n_ones - n_zeros) <= 1
-        g       = group s
-        b       = map (\x -> length x) g
+        b       = map (\x -> length x) (group s)
         n       = takeWhile (> 0) $ map (\x -> length $ elemIndices x b) [1..]
-        n_c2    = dropWhile (\x -> fst x == 2*snd x) (zip n (snd (splitAt 1 n))) 
+        n_c2    = dropWhile (\x -> fst x >= 2*snd x) (zip n (snd (splitAt 1 n))) 
         cond2   = length n_c2 <= 1
