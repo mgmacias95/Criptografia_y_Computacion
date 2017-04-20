622aad2ee0fdf20ec933834eab71206d6f45a7e7
mgmacias95 <mgmacias95@correo.ugr.es>
2017-04-20 11:14:48 +0100

    segundo postulado

--- CifradoFlujo.hs
+++ CifradoFlujo.hs
@@ -6,9 +6,13 @@ Escribe una función que determine si una secuencia de bits cumple con los
 postulados de Golomb.
 -}
 golomb :: (Integral a) => [a] -> Bool
-golomb s = cond1
+golomb s = cond1 && cond2
     where
         n_ones  = fromIntegral $ sum s
         n_zeros = (length s) - n_ones
         cond1   = abs (n_ones - n_zeros) <= 1
-
+        g       = group s
+        b       = map (\x -> length x) g
+        n       = takeWhile (> 0) $ map (\x -> length $ elemIndices x b) [1..]
+        n_c2    = dropWhile (\x -> fst x == 2*snd x) (zip n (snd (splitAt 1 n))) 
+        cond2   = length n_c2 <= 1
