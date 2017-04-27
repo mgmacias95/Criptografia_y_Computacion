3c985123303d5b1f96dd1b4e38ac9a791618e161
mgmacias95 <mgmacias95@correo.ugr.es>
2017-04-27 10:31:24 +0200

    2nd exercice done

--- CifradoFlujo.hs
+++ CifradoFlujo.hs
@@ -1,23 +1,36 @@
 import Data.List
 import Data.Bits
 
+-- ternary operator
+data Cond a = a :? a
+ 
+infixl 0 ?
+infixl 1 :?
+ 
+(?) :: Bool -> Cond a -> a
+True  ? (x :? _) = x
+False ? (_ :? y) = y
+
 {-
 Ejercicio 1.
 Escribe una función que determine si una secuencia de bits cumple con los 
 postulados de Golomb.
 -}
 golomb :: (Integral a) => [a] -> Bool
-golomb s = cond1 && cond2 && cond3
-    where
-        n_ones  = fromIntegral $ sum s
-        n_zeros = (length s) - n_ones
-        cond1   = abs (n_ones - n_zeros) <= 1
-        b       = map (\x -> length x) (group s)
-        n       = takeWhile (> 0) $ map (\x -> length $ elemIndices x b) [1..]
-        n_c2    = dropWhile (\x -> fst x >= 2*snd x) (zip n (snd (splitAt 1 n))) 
-        cond2   = length n_c2 <= 1
-        dists   = map (\x -> hamming_distance_one s x) [1..length s-1]
-        cond3   = (foldl1 (\ x y -> y - x) dists) == 0
+golomb s 
+    | length s == 1 = True
+    | otherwise     = cond1 && cond2 && cond3
+        where
+            s'      = head s == last s ? rotar s 1 :? s
+            n_ones  = fromIntegral $ sum s
+            n_zeros = (length s) - n_ones
+            cond1   = abs (n_ones - n_zeros) <= 1
+            b       = map (\x -> length x) (group s')
+            n       = takeWhile (> 0) $ map (\x -> length $ elemIndices x b) [1..]
+            n_c2    = dropWhile (\x -> fst x >= 2*snd x) (zip n (snd (splitAt 1 n))) 
+            cond2   = length n_c2 <= 1
+            dists   = map (\x -> hamming_distance_one s x) [1..length s-1]
+            cond3   = (foldl1 (\ x y -> y - x) dists) == 0
 
 
 rotar :: (Integral a) => [a] -> Int -> [a]
