3bcd7b31ec5e5ec1e57685076a677f9974ce72c0
mgmacias95 <mgmacias95@correo.ugr.es>
2017-05-17 12:05:03 +0200

    remove comment

--- CifradoFlujo.hs
+++ CifradoFlujo.hs
@@ -2,7 +2,6 @@ import Data.List
 import Data.Bits
 import Data.Digits (digits, unDigits)
 import Data.Char (chr, ord)
--- import Codec.Binary.UTF8.String (encode, decode)
 
 -- ternary operator
 data Cond a = a :? a
